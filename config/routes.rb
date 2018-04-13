259ef80d5fb723d8b2c804b58e5cd71cc6286444
yatheesh <yatheesh.kalimili@gmail.com>
2018-04-13 10:39:18 +0530

    Navigating pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/features'
+
+  get 'welcome/login'
+
   resources :invoices
+root to:'invoices#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
