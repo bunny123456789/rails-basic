e949f65072454d49ccac2363e7e99a4d3245bdff
yatheesh <yatheesh.kalimili@gmail.com>
2018-04-13 11:14:22 +0530

    updated navigation

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get 'welcome/login'
 
   resources :invoices
-root to:'invoices#index'
+root to:'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
